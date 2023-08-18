94235bf091e0655da177b864d34724cee2983a7f
rajendrakamath <mrkamath@gmail.com>
2023-08-18 15:00:08 +0530

    added tests

--- src/test/java/getUsers/GetAllUsersTest.java
+++ src/test/java/getUsers/GetAllUsersTest.java
@@ -1,7 +1,11 @@
 package getUsers;
 
 import baseTest.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import users.Data;
+import users.Support;
+import users.Users;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -20,7 +24,53 @@ public class GetAllUsersTest extends BaseTest {
                 .body("per_page", equalTo(6))
                 .body("total", equalTo(12))
                 .body("total_pages", equalTo(2))
-                .body("data", notNullValue())
+                .body("data.size()", equalTo(6))
                 .body("support", notNullValue());
+    }
+
+    @Test
+    public void verifyUserData() {
+        given()
+                .spec(spec)
+                .when()
+                .get("users")
+                .then()
+                .statusCode(200)
+                .body("data.id", everyItem(greaterThanOrEqualTo(1)))
+                .body("data.email", everyItem(notNullValue()))
+                .body("data.first_name", everyItem(notNullValue()))
+                .body("data.last_name", everyItem(notNullValue()))
+                .body("data.avatar", everyItem(notNullValue()));
+    }
+
+    @Test
+    public void verifyAllUsersWithPojo() {
+        Users users = given()
+                .spec(spec)
+                .when()
+                .get("users")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response()
+                .as(Users.class);
+        Assert.assertEquals(users.getPage(), 1);
+        Assert.assertEquals(users.getPer_page(), 6);
+        Assert.assertEquals(users.getTotal(), 12);
+        Assert.assertEquals(users.getTotal_pages(), 2);
+
+        Data[] data = users.getData();
+        for (Data eachData : data) {
+            Assert.assertTrue(eachData.getId() >= 1);
+            Assert.assertNotNull(eachData.getEmail());
+            Assert.assertNotNull(eachData.getFirst_name());
+            Assert.assertNotNull(eachData.getLast_name());
+            Assert.assertNotNull(eachData.getAvatar());
+        }
+
+        Support support = users.getSupport();
+        Assert.assertNotNull(support.getUrl());
+        Assert.assertNotNull(support.getText());
+
     }
 }
