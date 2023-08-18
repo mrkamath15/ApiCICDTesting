21a96b9855420591d3d8409ebd0247f59ce102e7
rajendrakamath <mrkamath@gmail.com>
2023-08-18 17:30:39 +0530

    added tests

--- src/test/java/getUsers/GetAllUsersTest.java
+++ src/test/java/getUsers/GetAllUsersTest.java
@@ -1,11 +1,17 @@
 package getUsers;
 
 import baseTest.BaseTest;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import users.Data;
 import users.Support;
 import users.Users;
+import utility.Constants;
+
+import java.io.File;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -71,6 +77,88 @@ public class GetAllUsersTest extends BaseTest {
         Support support = users.getSupport();
         Assert.assertNotNull(support.getUrl());
         Assert.assertNotNull(support.getText());
-
     }
+
+    @Test
+    public void getDataAsArray() {
+        Response response = given()
+                .spec(spec)
+                .when()
+                .get("users")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        Data[] data  = response.jsonPath().getObject("data", Data[].class);
+
+        for (Data eachData : data) {
+            Assert.assertTrue(eachData.getId() >= 1);
+            Assert.assertNotNull(eachData.getEmail());
+            Assert.assertNotNull(eachData.getFirst_name());
+            Assert.assertNotNull(eachData.getLast_name());
+            Assert.assertNotNull(eachData.getAvatar());
+        }
+    }
+
+    @Test
+    public void getDataAsList() {
+        Response response = given()
+                .spec(spec)
+                .when()
+                .get("users")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        List<Data> data = response.jsonPath().getList("data", Data.class);
+
+        for (Data eachData : data) {
+            Assert.assertTrue(eachData.getId() >= 1);
+            Assert.assertNotNull(eachData.getEmail());
+            Assert.assertNotNull(eachData.getFirst_name());
+            Assert.assertNotNull(eachData.getLast_name());
+            Assert.assertNotNull(eachData.getAvatar());
+        }
+    }
+
+    @Test
+    public void getUsersResponseTest() {
+        Response response = given()
+                .spec(spec)
+                .when()
+                .get("users")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+        System.out.println("Total : " + response.path("total").toString());
+        System.out.println("FirstName : " + response.path("data[0].first_name"));
+        System.out.println("Response Time : " + response.getTime());
+        System.out.println("Status Code : " + response.statusCode());
+        System.out.println("Headers : " + response.getHeaders());
+        System.out.println(response.jsonPath().getString("page"));
+    }
+
+    @Test
+    public void getUsersResponseTimeTest() {
+        given()
+                .spec(spec)
+                .when()
+                .get("users")
+                .then()
+                .statusCode(200)
+                .time(lessThan(700L));
+    }
+
+    @Test
+    public void getAllUsersSchemaValidationTest() {
+        given()
+                .spec(spec)
+                .when()
+                .get("users")
+                .then()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchema(new File(Constants.GET_ALL_USERS_SCHEMA)));
+    }
+
 }
---
+++ src/test/java/utility/Constants.java
@@ -0,0 +1,5 @@
+package utility;
+
+public class Constants {
+    public static final String GET_ALL_USERS_SCHEMA = System.getProperty("user.dir") + "//src//test//resources//schemas//GetAllUsers.json";
+}
